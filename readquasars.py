8ee147fe14fa0d3e3fea5e1f746b86251bad0d1e
LeeRoy Borders III <40214412+RoyBorders@users.noreply.github.com>
2018-06-15 16:16:35 -0400

    created functions to be able to read both quasar catalog
    some functtions not working

--- readquasars.py
+++ readquasars.py
@@ -1,1 +1,20 @@
 # this is to read the quasar catalog
+from astroquery.sdss import SDSS
+import numpy as np
+from astropy.table import Table
+def readquasarcatalog():
+
+	quasars = Table.read('/Users/lborders/data/DR14Q_v4_4.fits')
+	return quasars
+
+def getspectrum(plate, fiber, mjd):
+	hdulist=SDSS.get_spectra(plate=plate, fiberID=fiber, mjd=mjd)[0]
+	spectrum=hdulist[1].data
+	return spectrum
+
+
+
+quasars = readquasarcatalog()
+print(quasars)
+spec = getspectrum(7666, 121, 57339)
+print(Table(spec))
